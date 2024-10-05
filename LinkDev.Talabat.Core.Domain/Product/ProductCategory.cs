a9befada21271697867a085302ab6dee6af5255e
HazemElbehary <hazemelbehary19@gmail.com>
2024-10-05 13:39:54 +0300

    Part 03 Product Module - Entities

---
+++ LinkDev.Talabat.Core.Domain/Product/ProductCategory.cs
@@ -0,0 +1,9 @@
+﻿using LinkDev.Talabat.Core.Domain.Common;
+
+namespace LinkDev.Talabat.Core.Domain.Product
+{
+	public class ProductCategory : BaseEntity<int>
+	{
+		public required string Name { get; set; }
+	}
+}
