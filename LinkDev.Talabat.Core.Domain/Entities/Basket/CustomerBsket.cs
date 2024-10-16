b3addd575082930a225662947d9672e3e2aab588
HazemElbehary <hazemelbehary19@gmail.com>
2024-10-16 09:54:50 +0300

    Part 10 Basket Module Entities

---
+++ LinkDev.Talabat.Core.Domain/Entities/Basket/CustomerBsket.cs
@@ -0,0 +1,10 @@
+﻿namespace LinkDev.Talabat.Core.Domain.Entities.Basket
+{
+	public class CustomerBsket
+	{
+        public required string Id { get; set; }
+
+        public IEnumerable<BasketItem> Items { get; set; } = new List<BasketItem>();
+
+    }
+}
